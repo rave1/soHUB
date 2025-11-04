5ef56f23325d5d8410f55417a6baa6fd4e4a7df3
Sam Sepiol <jakubsopel7@gmail.com>
2025-11-04 19:03:34 +0100

    working flask example

--- app.py
+++ app.py
@@ -5,4 +5,10 @@ app = Flask(__name__)
 
 @app.route("/temp", methods=["POST"])
 def temp():
-    print(request.data)
+    print("---- NEW REQUEST ----")
+    print("Headers:", dict(request.headers))
+    print("Raw body:", request.data)
+    print("Form data:", request.form)
+    print("JSON:", request.get_json(silent=True))
+    print("----------------------", flush=True)
+    return "Good!", 201
