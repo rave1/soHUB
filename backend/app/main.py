44c0af8bbd29b4e1d01a8d862a319c96fcd73b8c
sam sepiol <jakubsopel7@gmail.com>
2025-12-28 13:25:41 +0100

    endpoint to get newest data

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 import os
 from fastapi import FastAPI
 import uvicorn
+import json
 from app.models import TempHumidity
 from app.clients.influx import InfluxDBClientWrapper
 from influxdb_client_3 import Point
@@ -22,6 +23,13 @@ async def temp(data: TempHumidity):
     )
     return {"status": "ok"}
 
+@app.get("/temp")
+async def get_data():
+    table = influx_client.query("SELECT room, temp, time, humidity FROM test WHERE time >= now() - interval '10 seconds' LIMIT 1")
+    df = table.to_pandas()
+    json_str = df.to_json(orient="records", date_format="iso", date_unit="s")
+    data = json.loads(json_str)
+    return data
 
 @app.get("/")
 async def hello():
